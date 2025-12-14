e20927e369bd001c3b8d2814f1cebd530b049459
23120102 <nnminhtuan2810@gmail.com>
2025-12-14 12:46:07 +0700

    thêm CSS cho header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 const Header = () => {
   return (
-    <header >
-      <div>
-          <p>23120102</p>
+    <header className="bg-red-100">
+      <div className="mx-auto flex max-w-[1200px] items-center justify-between px-4 py-2 text-red-800">
+          <p className="text-sm font-medium">23120102</p>
 
-          <h1>
+          <h1 className="text-lg font-semibold">
             Movies info
           </h1>
 
-          <button>
+          <button className="text-lg">
             ⚙️
           </button>
       </div>
