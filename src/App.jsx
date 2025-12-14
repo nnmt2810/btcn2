8976d77651bfa763cb48eb45cda7321dd7443c45
23120102 <nnminhtuan2810@gmail.com>
2025-12-14 13:04:27 +0700

    khởi tạo navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header />
+      <Navbar />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <button>
+                    🏠
+                </button>
+
+                <form>
+                    <input
+                        type="text"
+                        placeholder="Search"
+                    />
+
+                    <button type="submit">
+                        Search
+                    </button>
+                </form>
+            </div>
+        </nav>
+  )
+}
+
+export default Navbar
