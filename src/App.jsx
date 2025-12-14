faecd681c767054b94d5a494237e851450787502
23120102 <nnminhtuan2810@gmail.com>
2025-12-14 14:26:06 +0700

    dùng useState và useEffect để cập nhật darkMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import './App.css'
 import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(() => {
+    const root = document.documentElement
+    root.classList.toggle("dark", darkMode)
+  }, [darkMode])
 
   return (
-    <>
-      <Header />
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-800 transition-colors">
+      <Header
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+      />
       <Navbar />
-    </>
+    </div>
   )
 }
 
