86033389bfbde83a12cedf5a6b4ad92de4f3c888
23120102 <nnminhtuan2810@gmail.com>
2025-12-14 12:42:56 +0700

    khởi tạo component header

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <header >
+      <div>
+          <p>23120102</p>
+
+          <h1>
+            Movies info
+          </h1>
+
+          <button>
+            ⚙️
+          </button>
+      </div>
+    </header>  
+  )
+}
+
+export default Header
