36f1834b0a621788858d04f17a391c72058315f0
23120102 <nnminhtuan2810@gmail.com>
2025-12-14 13:19:41 +0700

    thêm CSS cho navbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 const Header = () => {
   return (
     <header className="bg-red-100">
-      <div className="mx-auto flex max-w-[1200px] items-center justify-between px-4 py-2 text-red-800">
+      <div className="mx-auto flex max-w-[1200px] h-16 items-center justify-between px-4 py-2 text-red-800">
           <p className="text-sm font-medium">23120102</p>
 
-          <h1 className="text-lg font-semibold">
+          <h1 className="text-2xl font-semibold">
             Movies info
           </h1>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,27 @@
 const Navbar = () => {
     return (
-        <nav>
-            <div>
-                <button>
+        <nav className=" bg-blue-100 shadow mt-2 py-2">
+            <div className="max-w-[1200px] h-8 mx-auto flex items-center justify-between gap-4 px-4 py-3">
+                <button
+                    type="button"
+                    className="rounded p-2 text-xl hover:bg-black transition"
+                    aria-label="Home"
+                >
                     🏠
                 </button>
 
-                <form>
+                <form className="flex items-center gap-2">
                     <input
                         type="text"
+                        name="search"
                         placeholder="Search"
+                        className="rounded border border-gray-300 px-3 py-2 outline-none focus:border-gray-400 focus:ring-1 focus:ring-red-300"
                     />
 
-                    <button type="submit">
+                    <button 
+                        type="submit"
+                        className="rounded bg-gray-300 text-black px-4 py-2 hover:bg-blue-600 transition"
+                    >
                         Search
                     </button>
                 </form>
